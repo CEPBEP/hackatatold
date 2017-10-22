3fa832ce67a20813fcb381b1b8a6d45d891de757
user <RumyantsevGT@ya.ru>
2017-10-22 11:25:58 +0300

    Update region.php

--- region.php
+++ region.php
@@ -8,7 +8,7 @@
         $rcode = $par["data"]["0"]["regionCode"];              // запоминаем в переменную(необязательно)
         echo '<pre>';print_r ($parsed_data);                   
         $fp = fopen ("test.txt", "a");                         // кладём переменную в текстовый файл
-        fwrite($fp,"$rcode\n");                                // дописываем файл с новой строки
+        fwrite($fp,"$rcode\n");                                // дописываем файл с новой строки(\)
         fclose($fp);                                           // закрываем файл
 ?>
 <?php       /*
